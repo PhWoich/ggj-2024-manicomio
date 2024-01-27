e5e9b76364fc3859df355d9f8c38692c73008bc8
heitordt <trevisolheitor@gmail.com>
2024-01-27 16:02:02 +0100

    Menu Principal

---
+++ Menus/ControlPopup.gd
@@ -0,0 +1,15 @@
+extends AcceptDialog
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_controls_button_pressed():
+	popup_centered_ratio(0.7)
