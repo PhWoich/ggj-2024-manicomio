0e3af2e33b204fe4a292eff53408492abfca0b23
PhWoich <pedrohenriquewoich@gmail.com>
2024-01-27 15:19:43 -0300

    Adicionado popup de dano

--- Cenas/Principal/cena_principal.gd
+++ Cenas/Principal/cena_principal.gd
@@ -4,7 +4,6 @@ class_name Cena_Principal
 var energia_maxima:float = 1000.0
 var energia_atual_gerada:float = 0.0
 var energia_atual_consumida:float = 0.0
-var energia_atual_disponivel:float = 0.0
 
 var custo_adicional:int
 var recurso:int = 100
@@ -32,15 +31,17 @@ func add_gerador(gerador:PackedScene, posicao:Vector2):
 	var instancia_gerador:Gerador_Basico = gerador.instantiate()
 	if recurso >= instancia_gerador.custo_base + custo_adicional:
 		instancia_gerador.position = posicao
+		instancia_gerador.gerador_destruido.connect(reduzir_energia_gerada)
 		energia_atual_gerada = energia_atual_gerada+instancia_gerador.energia_gerada
 		recurso = recurso-(instancia_gerador.custo_base + custo_adicional)
 		custo_adicional = custo_adicional+1
 		add_child(instancia_gerador)
-		print(recurso)
-	else :
-		print(instancia_gerador.custo_base + custo_adicional)
 
 func gerar_ataque_dist_jogador(posicao, movimento, rotacao):
 	var instancia_torta = torta_na_cara_jogador.instantiate()
 	instancia_torta.inicializar(posicao,movimento, rotacao)
 	add_child(instancia_torta)
+
+func reduzir_energia_gerada(quantidade):
+	energia_atual_gerada = energia_atual_gerada-quantidade
+	print(energia_atual_gerada)
