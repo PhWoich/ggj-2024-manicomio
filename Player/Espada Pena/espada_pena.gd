300910842e736104a5e7ed42f7b3112d866f2b52
PhWoich <pedrohenriquewoich@gmail.com>
2024-01-27 02:44:52 -0300

    Inclusão do ataque melee do jogador
    Incluído o ataque corpo-a-copor do jogador, que salva qual a ultima posição que o jogador olhou e muda o giro com base para qual lado está olhando

---
+++ Player/Espada Pena/espada_pena.gd
@@ -0,0 +1,32 @@
+extends Area2D
+class_name Espada_Pena
+
+signal ataque_encerrado
+
+var direcao_jogador_olhando
+@export var velocidade_rotacao = 0.1 
+# -45° em radianos
+var rotacao_inicial:float = -0.785398
+var ponto_partida_ataque
+var girar_animacao:bool
+
+func inicializar(ponto_partida:Node2D, girar_anim:bool):
+	rotacao_inicial = ponto_partida.rotation
+	rotation = rotacao_inicial
+	girar_animacao=girar_anim
+	ponto_partida_ataque = ponto_partida
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if girar_animacao:
+		rotation = rotation-velocidade_rotacao
+		# Quando a tiver rodado 180° em radiano
+		if rotation-rotacao_inicial <= -3.14159:
+			emit_signal("ataque_encerrado")
+			queue_free()
+	else:
+		rotation = rotation+velocidade_rotacao
+		# Quando a tiver rodado 180° em radiano
+		if rotation-rotacao_inicial >= 3.14159:
+			emit_signal("ataque_encerrado")
+			queue_free()
